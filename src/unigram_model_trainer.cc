83e5ff1d2ee7ae90f53dfe97b4dbada186cfb8ff
Wissam Antoun <44616226+WissamAntoun@users.noreply.github.com>
2020-02-11 16:24:18 +0200

    Update unigram_model_trainer.cc

--- src/unigram_model_trainer.cc
+++ src/unigram_model_trainer.cc
@@ -107,8 +107,9 @@ TrainerModel::SentencePieces Trainer::MakeSeedSentencePieces() const {
   constexpr char32 kSentenceBoundary = 0x0000;
   int64 ii=0;
   for (const auto &w : sentences_) {
-    if(ii++%1000000)
+    if(ii++%1000000){
       LOG(INFO) << " Merged "<< ii << " sentences";
+    }
     for (const auto &c : string_util::UTF8ToUnicodeText(w.first)) {
       array.push_back(c);
       if (c != kUNKChar && c != kSentenceBoundary) {
